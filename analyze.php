22d693becf56767ff585f788d3e5f79d21d03018
datagutten <datagutten@datagutten.net>
2013-05-26 20:15:59 +0200

    Ny analyze.php
    Ny analyze.php som hvor alt er pakket inn en funksjon som kan kalles fra
    et script som henter bildet (mail/web-upload osv).

---
+++ analyze.php
@@ -0,0 +1,72 @@
+<?Php
+function analyze($imagefile)
+{
+	
+	if(!file_exists($imagefile))
+		die("Finner ikke $imagefile\n");
+	require 'information/information.php';
+	require 'prepare/prepare.php';
+	require 'information/debug.php';
+	
+	$information=new information($imagefile);
+	$prepare=new prepare($information->folder);
+	
+	
+	//$information->debug=true;
+	//$prepare->debug=true;
+	//$debug=new debug;
+	
+	$rack=$prepare->tilesplitter($information->inputim,$information->rackxlist,$information->sizes->racktilesize,$information->sizes->racky); //Hent spillerens brikker
+	
+	if(isset($debug))
+	{
+		$debug->displayimages($rack,'rack');
+		echo '<br>';
+	}
+	
+	$letters=array();
+	foreach($information->ylist as $ykey=>$y)
+	{
+		$images=$prepare->tilesplitter($information->inputim,$information->xlist,$information->sizes->tilesize,$y); //Hent en rad med brikker
+		if(isset($debug))
+		{
+			$debug->displayimages($images,$y);
+			//$allimages[$y]=$images;
+			echo '<br>';
+		}
+		$templetters=$prepare->sorttiles($images,$information->folder,$ykey);
+		if($templetters!==false) //Bare legg til linjer som inneholder bokstaver
+			$letters=$letters+$templetters;
+	}
+	
+	
+	if(isset($debug))
+	{
+		$debug->displayimages($letters);
+		echo '<br>';
+	}
+	
+	include 'getblack.php';
+	
+	foreach ($letters as $key=>$letter)
+	{
+		list($blackletters[$key],$blankletters[$key])=getblack($letter);
+		imagepng($blackletters[$key],$information->folder."letters_black/$key.png");
+		$ocrletters[$key]=$prepare->ocr($information->folder."letters_black/$key.png",$information->folder."letters_ocr/$key");
+	}
+	if(isset($debug))
+	{
+		$debug->displayimages($blackletters,'black',$ocrletters);
+		var_dump($blankletters);
+	}
+	$tiles=$information->tiles;
+		foreach ($ocrletters as $key=>$letter)
+		{
+			if(!$blankletters[$key])
+				$tiles[$letter]--;
+			else
+				$tiles['blank']--;
+		}
+		return $tiles;
+}
+?>
